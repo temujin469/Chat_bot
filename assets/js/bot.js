1ecd1672be57e5a3db4f39fc76bca64169578b63
temujin469 <ttemujin469@gmail.com>
2022-01-14 18:20:54 +0800

    v 1.0.0

---
+++ assets/js/bot.js
@@ -0,0 +1,17 @@
+function getBotResponse(userValue){
+    if(userValue == 'sain uu'){
+        return 'sain sain uu';
+    }else if( userValue == 'yu bainda'){
+        return 'hicheele hiigel baijinda';
+    }
+    else if(userValue == 'bayrtai'){
+        return 'bayrtai saihn ammrai';
+    }
+    else if(userValue == 'soko'){
+        return '<a href="https://www.instagram.com/slngotstsg/">temuujingiin face</a>';
+    }
+    else{
+        return 'haha zaaz😂';
+    }
+
+}
